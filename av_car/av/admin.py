f47ed26120ac136ff55821453761d33c59f3a958
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-06-08 08:13:54 +0300

    обновление админки

--- av_car/av/admin.py
+++ av_car/av/admin.py
@@ -26,3 +26,41 @@ class CategoryAdmin(admin.ModelAdmin):
 class CarAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'price',)
     search_fields = ('id', 'name')
+
+
+@admin.register(BotUser)
+class BotUserAdmin(admin.ModelAdmin):
+    list_display = get_fields_for_model(BotUser)
+    search_fields = ['telegram_id', 'username', 'first_name', 'last_name']
+    list_filter = ['telegram_id', 'username', 'first_name', 'last_name']
+
+
+@admin.register(TelegramChat)
+class TelegramChatAdmin(admin.ModelAdmin):
+    list_display = get_fields_for_model(TelegramChat)
+    list_filter = ['name']
+
+
+@admin.register(InviteLink)
+class InviteLinkAdmin(admin.ModelAdmin):
+    list_display = get_fields_for_model(InviteLink)
+    list_filter = ['telegram_chat', 'name']
+
+
+@admin.register(TelegramSubscriber)
+class TelegramSubscriberAdmin(admin.ModelAdmin):
+    list_display = get_fields_for_model(TelegramSubscriber)
+    list_filter = ['telegram_id']
+
+
+@admin.register(NewMessage)
+class NewMessageAdmin(admin.ModelAdmin):
+    list_display = get_fields_for_model(NewMessage)
+    list_editable = ['message', 'start_time', 'end_time']
+
+
+@admin.register(AskMessage)
+class AskMessage(admin.ModelAdmin):
+    list_display = get_fields_for_model(AskMessage)
+    list_editable = ['answer']
+    list_filter = get_fields_for_model(AskMessage)
