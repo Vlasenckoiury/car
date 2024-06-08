774a040e829f68c903acf0333e9b8e4218af8994
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-06-08 08:06:30 +0300

    добавление модели телеграм

--- av_car/av/models.py
+++ av_car/av/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Category(models.Model):
@@ -36,3 +37,124 @@ class Car(models.Model):
 
     def __str__(self):
         return self.name
+
+class BotUser(models.Model):
+    telegram_id = models.PositiveBigIntegerField(_('ID Telegram'), db_index=True, unique=True)
+    username = models.CharField(_('Username'), max_length=100, blank=True, null=True)
+    first_name = models.CharField(_('Имя'), max_length=100, blank=True, null=True)
+    last_name = models.CharField(_('Фамилия'), max_length=100, blank=True, null=True)
+    contact = models.CharField(_('Телефон'), max_length=100, blank=True, null=True)
+
+    class Meta:
+        app_label = "av_car"
+        verbose_name = 'Пользователь бота'
+        verbose_name_plural = 'Пользователи бота'
+
+    def __str__(self):
+        return f'{self.telegram_id} {self.username}'
+
+
+class TelegramChat(models.Model):
+    name = models.CharField(_('Имя канала'),
+                            max_length=150, blank=True, null=True, help_text='Имя Канала')
+    chat_link = models.CharField(_('Ссылка канала'),
+                                 max_length=150, db_index=True, help_text='Пример ссылки: https://t.me/test')
+
+    def __str__(self):
+        return f'{self.chat_link} {self.name}'
+
+    class Meta:
+        verbose_name = 'Телеграм канал'
+        verbose_name_plural = 'Телеграм каналы'
+
+
+class InviteLink(models.Model):
+    # Пригласительные ссылки
+    telegram_chat = models.ForeignKey(TelegramChat, verbose_name=_('Телеграм канал'), on_delete=models.CASCADE)
+    creates_join_request = models.BooleanField(_('Запрос на добавление'), default=True)
+    creator = models.CharField(_('Создатель'), max_length=250, blank=True, null=True)
+    expire_date = models.CharField(_('expire_date'), max_length=150, blank=True, null=True)
+    link = models.CharField(_('Ссылка'), max_length=150, blank=True, null=True)
+    is_primary = models.BooleanField(_('Is_primary'), blank=True, null=True)
+    is_revoked = models.BooleanField(_('Is_revoked'), blank=True, null=True)
+    member_limit = models.IntegerField(_('Лимит подписок'), blank=True, null=True)
+    name = models.CharField(_('name'), max_length=150, blank=True, null=True)
+    pending_join_request_count = models.IntegerField(_('pending_join_request_count'), blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.link}'
+
+    class Meta:
+        verbose_name = 'Пригласительная ссылка'
+        verbose_name_plural = 'Пригласительные ссылки'
+
+
+class TelegramSubscriber(models.Model):
+    invite_link = models.ForeignKey(TelegramChat, verbose_name=_('Канал'), on_delete=models.CASCADE)
+    telegram_id = models.PositiveBigIntegerField(_('ID Telegram'), db_index=True, unique=True)
+    username = models.CharField(_('Username'), max_length=100, blank=True, null=True)
+    first_name = models.CharField(_('Имя'), max_length=100, blank=True, null=True)
+    last_name = models.CharField(_('Фамилия'), max_length=100, blank=True, null=True)
+    subcribed = models.BooleanField(_('Подписан'), default=False)
+    datetime_subscribe = models.DateTimeField(_('Время подписки'), blank=True, null=True)
+    datetime_unsubscribe = models.DateTimeField(_('Время отписки'), blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.telegram_id}: {self.username}'
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+
+
+class NewMessage(models.Model):
+    link_chat = models.ForeignKey(TelegramChat, on_delete=models.CASCADE, verbose_name=_('Канал'), related_name='messages')
+    message = models.TextField((_('Новость')))
+    start_time = models.TimeField(_('Время начало'), default=None, blank=True, null=False, help_text='Пример ввода времени: 10:00:00')
+    end_time = models.TimeField(_('Время конца'), default=None, blank=True, null=False, help_text='Пример ввода времени: 10:00:00')
+    created_at = models.DateTimeField(_('Время добавления новости'), blank=True, null=True, auto_now_add=True)
+    day_monday = models.BooleanField(_('Понедельник'), default=False)
+    day_tuesday = models.BooleanField(_('Вторник'), default=False)
+    day_wednesday = models.BooleanField(_('Среда'), default=False)
+    day_thursday = models.BooleanField(_('Четверг'), default=False)
+    day_friday = models.BooleanField(_('Пятница'), default=False)
+    day_saturday = models.BooleanField(_('Суббота'), default=False)
+    day_sunday = models.BooleanField(_('Воскресенье'), default=False)
+
+    def boolean_to_int(self):
+        days = {
+            self.day_monday: 0,
+            self.day_tuesday: 1,
+            self.day_wednesday: 2,
+            self.day_thursday: 3,
+            self.day_friday: 4,
+            self.day_saturday: 5,
+            self.day_sunday: 6
+        }
+        res = [value for day, value in days.items() if day]
+        if res:
+            return res[0]
+        else:
+            return res
+
+    def __str__(self):
+        return f'{self.message}'
+
+    class Meta:
+        verbose_name = 'Новости и расписание канала'
+        verbose_name_plural = 'Новости и расписание каналов'
+
+
+class AskMessage(models.Model):
+    chat_id = models.BigIntegerField(_('Чат Id'), default=False, blank=True, null=True)
+    question = models.TextField(_('Вопрос'), default=None, blank=True, null=True)
+    answer = models.TextField(_('Ответ'), default=False, blank=True, null=True)
+    result = models.BooleanField(_('Результат'), default=False, help_text='Отметьте если ответили на вопрос', blank=True, null=True)
+    created_at = models.DateTimeField(_('Время добавления'), blank=True, null=True, auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.question}'
+
+    class Meta:
+        verbose_name = 'Вопрос и Ответ'
+        verbose_name_plural = 'Вопросы и Ответы'
